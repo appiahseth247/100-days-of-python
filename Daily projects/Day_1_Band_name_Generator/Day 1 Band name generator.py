0aeb61f6c4d063815a74474eab9994cd266951ed
appiahseth247 <appiahseth247@gmail.com>
2022-06-25 06:46:19 +0900

    complete band name generator

---
+++ Daily projects/Day_1_Band_name_Generator/Day 1 Band name generator.py
@@ -0,0 +1,16 @@
+"""
+********************************************************************
+*    Course: 100 Days of Code - Dr. Angela Yu                      *
+*    Author: Seth Appiah                                           *
+*    Day: 1 - Band Name Generator                                  *
+*    Subject: Printing, Commenting, Debugging, Strings, Variables  *
+*    Date: 2022-02-09                                              *
+********************************************************************
+"""
+
+# This code asks the user for name of city and pet and then combines them to form the name of the band
+name = input('What is your names?\n')
+print('Hello ' + name + ', Welcome to the band name generator.')
+city = input('In what city did you grow up?\n')
+pet = input('What is the name of your pet?\n')
+print('The name of your band is ' + city + ' ' + pet)
