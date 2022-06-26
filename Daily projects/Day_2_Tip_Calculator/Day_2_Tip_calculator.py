3dae84869cb9ea83dd359d1870820e872f163b66
appiahseth247 <appiahseth247@gmail.com>
2022-06-26 14:47:48 +0900

    Exercise 2.1 Add numbers in a two_digit number

---
+++ Daily projects/Day_2_Tip_Calculator/Day_2_Tip_calculator.py
@@ -0,0 +1,23 @@
+"""
+*****************************************************************
+*    Course: 100 Days of Code - Dr. Angela Yu                   *
+*    Author: Seth Appiah                                        *
+*    Day: 2 - Tip Calculator                                    *
+*    Subject: Data types, f-string                              *
+*    Date: 2022-02-10                                           *
+*****************************************************************
+"""
+
+# Exercise 2.1
+# Instructions
+# Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+type(two_digit_number)
+numb =(two_digit_number)
+results = print(int(numb[0]) + int(numb[1]))
+
