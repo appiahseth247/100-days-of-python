9be7d9cdee59680e017f4276c622c221a2c453be
appiahseth247 <appiahseth247@gmail.com>
2022-06-26 14:37:34 +0900

    initial commit

---
+++ Daily projects/Day_2_Tip_Calculator/Day_2_Tip_calculator.py
@@ -0,0 +1,9 @@
+"""
+*****************************************************************
+*    Course: 100 Days of Code - Dr. Angela Yu                   *
+*    Author: Seth Appiah                                        *
+*    Day: 2 - Tip Calculator                                    *
+*    Subject: Data types, f-string                              *
+*    Date: 2022-02-10                                           *
+*****************************************************************
+"""
