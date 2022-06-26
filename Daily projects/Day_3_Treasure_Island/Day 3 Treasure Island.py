b7180a7e036063c3c40792754e565ed58afa31c8
appiahseth247 <appiahseth247@gmail.com>
2022-06-26 18:37:03 +0900

    Initial commit

---
+++ Daily projects/Day_3_Treasure_Island/Day 3 Treasure Island.py
@@ -0,0 +1,10 @@
+"""
+*************************************************************************************************************
+*    Course: 100 Days of Code - Dr. Angela Yu                                                               *
+*    Author: Seth Appiah                                                                                    *
+*    Day: 3 - Treasure Island                                                                               *
+*    Subject: conditional statements, logical operators, Code blocks and Scope                              *
+*    Date: 2022-02-13                                                                                       *
+*************************************************************************************************************
+"""
+
