6abe4bee4cbf2b0d52a433869d3390c769d7e4d2
appiahseth247 <appiahseth247@gmail.com>
2022-07-10 07:07:43 +0900

    Exercise 5.3 - Adding Even Numbers

--- Daily projects/Day_5_Password_Generator1/Day 5 password generator.py
+++ Daily projects/Day_5_Password_Generator1/Day 5 password generator.py
@@ -8,22 +8,24 @@
 *************************************************************************************************************
 """
 
-# Exercise 5.2 - High Score
+# Exercise 5.3 - Adding Even Numbers
 
 """Instructions
-You are going to write a program that calculates the highest score from a List of scores.
-Important you are not allowed to use the max or min functions.
-"""
-# 🚨 Don't change the code below 👇
-student_scores = input("Input a list of student scores ").split()
-for n in range(0, len(student_scores)):
-    student_scores[n] = int(student_scores[n])
-print(student_scores)
-# 🚨 Don't change the code above 👆
+You are going to write a program that calculates the sum of all the even numbers from 1 to 100. Thus, the first even number would be 2 and the last one is 100:
 
+i.e. 2 + 4 + 6 + 8 +10 ... + 98 + 100
+
+Important, there should only be 1 print statement in your console output. It should just print the final total and not every step of the calculation.
+"""
 # Write your code below this row 👇
-highest_score = 0
-for score in student_scores:
-    if score > highest_score:
-        highest_score = score
-print(f"The highest score in the class is: {highest_score}")
+
+total_sum = 0
+for number in range(0, 101, 2):
+    total_sum += number
+print(total_sum)
+
+sums = 0
+for number in range(0, 101):
+    if number % 2 == 0:
+        sums += number
+print(sums)
