7f5b40bd8b9660addad3dd7cd395fef5b899dd66
appiahseth247 <appiahseth247@gmail.com>
2022-07-10 22:53:19 +0900

    Exercise 6.4 - Reeborg's world Hurdle 4

--- Daily projects/Day_6_Escaping_Maze/Day 6 Escaping the maze.py
+++ Daily projects/Day_6_Escaping_Maze/Day 6 Escaping the maze.py
@@ -8,9 +8,34 @@
 *************************************************************************************************************
 The code below will only run at Reeborg's world website. So follow the link below to run the code there.
 Just copy and paste
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
 """
-# Exercise 6.3 - Reeborg's world Hurdle 3
-# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+# Exercise 6.4 - Reeborg's world Hurdle 4
+"""
+def turn_right():
+
+    turn_left()
+
+    turn_left()
+
+    turn_left()
+
+while not at_goal():
+
+    if front_is_clear():
+
+        move()
+
+        if right_is_clear():
+
+            turn_right()
+
+    else:
+
+        turn_left()
+"""
+# my code below which works perfectly for most cases but not others.
+
 """
 def turn_right():
     turn_left()
@@ -18,17 +43,22 @@ def turn_right():
     turn_left()
 
 def jump():
-    turn_left()
-    move()
     turn_right()
     move()
     turn_right()
     move()
-    turn_left()
 
 while not at_goal():
-    if wall_in_front():
-        jump()
-    else:
+    while front_is_clear():
         move()
+    if wall_in_front:
+        turn_left()
+    while wall_on_right():
+        move()
+    if right_is_clear():
+        jump()
+    while front_is_clear():
+        move()
+    if wall_in_front():
+        turn_left()
 """
