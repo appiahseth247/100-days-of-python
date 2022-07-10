6f4278be7e68aa640f3ed80b82761f57c07a1666
appiahseth247 <appiahseth247@gmail.com>
2022-07-10 22:39:59 +0900

    Day 6 Project - Escaping the Maze

---
+++ Daily projects/Day_6_Escaping_Maze/Day 6 Escaping the maze.py
@@ -0,0 +1,31 @@
+"""
+*************************************************************************************************************
+*    Course: 100 Days of Code - Dr. Angela Yu                                                               *
+*    Author: Seth Appiah                                                                                    *
+*    Day: 6 - Escaping the Maze                                                                             *
+*    Subject: Functions, While Loops, Code block                                                            *
+*    Date: 2022-02-27                                                                                       *
+*************************************************************************************************************
+The code below will only run at Reeborg's world website. So follow the link below to run the code there.
+Just copy and paste
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+"""
+# Day 6 Project - Escaping the Maze
+
+"""
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+while front_is_clear():
+    move()
+turn_left()
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+"""
