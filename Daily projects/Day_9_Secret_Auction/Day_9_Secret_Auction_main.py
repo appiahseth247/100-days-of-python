4e06d4747b1ed3bca37db539a5832a8d1c72c809
appiahseth247 <appiahseth247@gmail.com>
2022-08-14 07:03:25 +0900

    Exercise 9.2 - Dictionary in List

--- Daily projects/Day_9_Secret_Auction/Day_9_Secret_Auction_main.py
+++ Daily projects/Day_9_Secret_Auction/Day_9_Secret_Auction_main.py
@@ -8,57 +8,46 @@
 *************************************************************************************************************
 """
 
-# Exercise 9.1 - Grading Program
+# Exercise 9.2 - Dictionary in List
 """
 Instructions
+You are going to write a program that adds to a travel_log. You can see a travel_log which is a List that contains 2 Dictionaries.
 
-You have access to a database of student_scores in the format of a dictionary. The keys in student_scores are the names of the students and the values are their exam scores.
+Write a function that will work with the following line of code on line 21 to add the entry for Russia to the travel_log.
 
-Write a program that converts their scores to grades. By the end of your program, you should have a new dictionary called student_grades that should contain student names for keys and their grades for values. The final version of the student_grades dictionary will be checked.
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+You've visited Russia 2 times.
 
-DO NOT modify lines 1-7 to change the existing student_scores dictionary.
+You've been to Moscow and Saint Petersburg.
 
-DO NOT write any print statements.
-
-This is the scoring criteria:
-
-Scores 91 - 100: Grade = "Outstanding"
-
-Scores 81 - 90: Grade = "Exceeds Expectations"
-
-Scores 71 - 80: Grade = "Acceptable"
-
-Scores 70 or lower: Grade = "Fail"
-
-Expected Output '{'Harry': 'Exceeds Expectations', 'Ron': 'Acceptable', 'Hermione': 'Outstanding', 
-'Draco': 'Acceptable', 'Neville': 'Fail'}' """
-
+DO NOT modify the travel_log directly. You need to create a function that modifies it.
+"""
 # Solution code
 
-student_scores = {
-    "Harry": 81,
-    "Ron": 78,
-    "Hermione": 99,
-    "Draco": 74,
-    "Neville": 62,
-}
-# 🚨 Don't change the code above 👆
+travel_log = [
+    {
+        "country": "France",
+        "visits": 12,
+        "cities": ["Paris", "Lille", "Dijon"]
+    },
+    {
+        "country": "Germany",
+        "visits": 5,
+        "cities": ["Berlin", "Hamburg", "Stuttgart"]
+    },
+]
 
-# TODO-1: Create an empty dictionary called student_grades.
-student_grades = {}
 
-# TODO-2: Write your code below to add the grades to student_grades.👇
-for student in student_scores:
-    score = student_scores[student]
-    if score > 90:
-        student_grades[student] = "Outstanding"
-    elif score > 80:
-        student_grades[student] = "Exceeds Expectations"
-    elif score > 70:
-        student_grades[student] = "Acceptable"
-    else:
-        student_grades[student] = "Fail"
+# 🚨 Do NOT change the code above
 
-# 🚨 Don't change the code below 👇
-print(student_grades)
+# TODO: Write the function that will allow new countries
+# to be added to the travel_log. 👇
+def add_new_country(country_visited, times_visited, cities_visited):
+    """This function add a new country data to the travel log"""
+    new_country = {"country": country_visited, "visits": times_visited, "cities": cities_visited}
+    travel_log.append(new_country)
 
+
+# 🚨 Do not change the code below
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
