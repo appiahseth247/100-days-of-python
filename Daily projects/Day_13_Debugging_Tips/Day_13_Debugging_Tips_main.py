86e9ff281b42bf90b5d9b894ff9e7a5249d9daf1
appiahseth247 <appiahseth247@gmail.com>
2022-08-16 10:15:46 +0900

    Day 13  project - Debugging

---
+++ Daily projects/Day_13_Debugging_Tips/Day_13_Debugging_Tips_main.py
@@ -0,0 +1,24 @@
+"""
+*************************************************************************************************************
+*    Course: 100 Days of Code - Dr. Angela Yu                                                               *
+*    Author: Seth Appiah                                                                                    *
+*    Day: 11 - Debugging                                                                                   *
+*    Subject: Tips for Debugging a code                                                         *
+*    Date: 2022-03-17                                                                                      *
+*************************************************************************************************************
+"""
+
+# Day 13  project - Debugging
+
+"""
+1.Describe the problem
+2. Reproduce the bug
+3. Play computer and evaluate each line
+4. Watch red underlines and fix error
+5. Squash bugs with a print() statement. Print a lot
+6. Use a debugger
+7. Take a break
+8. Ask a friend
+9. Run the code often
+10. Ask StackOverflow
+"""
