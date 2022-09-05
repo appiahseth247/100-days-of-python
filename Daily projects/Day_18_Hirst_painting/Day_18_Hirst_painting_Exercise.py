bf367a2b3e0e8fda2166c878b490a36b7ed6f1ad
appiahseth247 <appiahseth247@gmail.com>
2022-09-05 10:53:49 +0900

    Exercise 18.4 - Generate a Random Walk

--- Daily projects/Day_18_Hirst_painting/Day_18_Hirst_painting_Exercise.py
+++ Daily projects/Day_18_Hirst_painting/Day_18_Hirst_painting_Exercise.py
@@ -17,15 +17,23 @@ tim = Turtle()
 # screen.register_shape("turtle.gif")  # to register my own turtle image
 # tim.shape("turtle.gif")
 tim.shape("turtle")
-turtle.colormode(255)
+turtle.colormode(255)  # To use the RGB format of coloring
 
-# Exercise 18.3 - Draw Shapes(polygons)
-colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
-for n in range(3, 11):
-    angle = 360/n
-    tim.color(random.choice(colors))
-    for i in range(n):
-        tim.right(angle)
-        tim.fd(100)
+
+# Exercise 18.Challenge 4 - Generate a Random Walk
+def rand_color():  # RGB
+    r = random.randint(0, 255)  # The higher the number the deeper the color generated
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)  # A tuple: immutable list
+    return random_color
+
+
+for n in range(150):
+    tim.color(rand_color())
+    tim.seth(random.choice([0, 90, 180, 270]))  # move toward a random direction (angle)
+    tim.speed(0)  # fastest
+    tim.pensize(10)
+    tim.fd(40)
 
 screen.exitonclick()
